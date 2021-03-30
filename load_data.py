5acbc7520aa7136c15d8d28f494eb3871863ea22
PaulinaPacyna <pacyna1998@gmail.com>
2021-03-31 00:01:25 +0200

    corrplots

--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 cancer = pd.read_csv('data/breast_cancer.csv')
 candy = pd.read_csv('data/candy.csv')
@@ -14,3 +15,13 @@ bankrupcy = pd.read_csv('data/bankrupt.csv')
 bankrupcy['EXP_CEO_NA'] = bankrupcy['EXP_CEO'].isna().astype(int)
 bankrupcy['EXP_CEO'] = bankrupcy['EXP_CEO'].replace(np.nan, bankrupcy['EXP_CEO'].mean())
 
+sns.heatmap(cancer.corr(), annot=True, fmt='.2f')
+plt.show()
+sns.heatmap(candy.corr(), annot=True, fmt='.2f')
+plt.show()
+sns.heatmap(nba.corr(), annot=True, fmt='.2f')
+plt.show()
+sns.heatmap(wine.corr(), annot=True, fmt='.2f')
+plt.show()
+sns.heatmap(bankrupcy.corr(), annot=True, fmt='.2f')
+plt.show()
