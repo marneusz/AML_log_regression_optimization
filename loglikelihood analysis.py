b3c05c51e3cb5378f3ea9fe15e7ebab281c4011c
PaulinaPacyna <pacyna1998@gmail.com>
2021-04-05 23:00:06 +0200

    loglikelihood

--- loglikelihood analysis.py
+++ loglikelihood analysis.py
@@ -1,20 +1,19 @@
-from log_regression import LogisticModel
+from log_regression import LogisticModel, predict_probabilities
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-epochs = 10, 50, #100, 500, 1000,
-nba = pd.read_csv(f'data/before decorrelation/nba.csv').drop(columns = 'Name')
-assessment = pd.DataFrame([], columns=['LL for GD', 'LL for SGD', 'LL for IRLS'])
+epochs = 10, 50, 100, 500, 1000,
+cancer = pd.read_csv(f'data/after preprocessing/breast_cancer.csv').drop(columns = ['Sample code number', 'Bare Nuclei'])
+assessment = pd.DataFrame([], columns=['LL for GD', 'LL for SGD'])
 for n_epochs in epochs:
-    GD = LogisticModel(nba.drop(columns = 'TARGET'), nba[['TARGET']])
-    SGD = LogisticModel(nba.drop(columns = 'TARGET'), nba[['TARGET']])
-    IRLS = LogisticModel(nba.drop(columns = 'TARGET'), nba[['TARGET']])
+    GD = LogisticModel(cancer.drop(columns ='Maligant'), cancer[['Maligant']])
+    SGD = LogisticModel(cancer.drop(columns ='Maligant'), cancer[['Maligant']])
+    #IRLS = LogisticModel(cancer.drop(columns = 'Target'), cancer[['Target']])
     GD.GD(n_epochs)
     SGD.SGD(n_epochs)
-    IRLS.IRLS(n_epochs, 0.1)
+    #IRLS.IRLS(n_epochs, 0.1)
     assessment = assessment.append({'LL for GD': GD.log_likelihood(),
-                       'LL for SGD': SGD.log_likelihood() ,
-                       'LL for IRLS': IRLS.log_likelihood()}, ignore_index=True)
+                       'LL for SGD': SGD.log_likelihood() }, ignore_index=True)
 
 plt.plot(epochs, assessment)
