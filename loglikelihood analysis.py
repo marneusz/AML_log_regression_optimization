cfd97e40faacce3019ba5e53a70481a22d2ae9d3
PaulinaPacyna <pacyna1998@gmail.com>
2021-04-08 19:08:28 +0200

    train test split, renaming

--- loglikelihood analysis.py
+++ loglikelihood analysis.py
@@ -1,4 +1,6 @@
-from log_regression import LogisticModel, predict_probabilities
+from sklearn.model_selection import train_test_split
+
+from log_regression import LogisticModel
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -7,20 +9,21 @@ dataset = pd.read_csv(f'data/after preprocessing/breast_cancer.csv')
 target_name = 'Maligant'
 X = dataset.drop(columns=target_name)
 y = dataset[[target_name]]
-
-assessment = pd.DataFrame([], columns=['Log-likelihood for GD', 'Log-likelihood for SGD', 'Log-likelihood for IRLS'])
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+loglikelihoods = pd.DataFrame([], columns=['Log-likelihood for GD', 'Log-likelihood for SGD', 'Log-likelihood for IRLS'])
 for n_epochs in epochs:
-    GD = LogisticModel(X, y)
-    SGD = LogisticModel(X, y)
-    IRLS = LogisticModel(X, y)
+    GD = LogisticModel(X_train, y_train)
+    SGD = LogisticModel(X_train, y_train)
+    IRLS = LogisticModel(X_train, y_train)
     GD.GD(n_epochs)
     SGD.SGD(n_epochs)
     IRLS.IRLS(n_epochs)
-    assessment = assessment.append({'Log-likelihood for GD': GD.log_likelihood(),
+    loglikelihoods = loglikelihoods.append({'Log-likelihood for GD': GD.log_likelihood(),
                                     'Log-likelihood for SGD': SGD.log_likelihood(),
                                     'Log-likelihood for IRLS': IRLS.log_likelihood()
-                                    }, ignore_index=True)
-for label in assessment.columns:
-    plt.plot(epochs, assessment[label], label=label)
-plt.ylim((assessment.min().min(), 0))
+                                            }, ignore_index=True)
+
+for label in loglikelihoods.columns:
+    plt.plot(epochs, loglikelihoods[label], label=label)
+plt.ylim((loglikelihoods.min().min(), 0))
 plt.legend()
